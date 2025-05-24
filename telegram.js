1b91484f35a2307d15e550e6d78bbd60f71d0be8
nai golf <niy_golf@hotmail.com>
2025-05-25 01:04:59 +0700

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,10 +1,13 @@
-// telegram.js
 const axios = require('axios');
 
 module.exports = async function sendTelegram(token, chatId, message) {
   const url = `https://api.telegram.org/bot${token}/sendMessage`;
-  await axios.post(url, {
-    chat_id: chatId,
-    text: message,
-  });
+  try {
+    await axios.post(url, {
+      chat_id: chatId,
+      text: message,
+    });
+  } catch (error) {
+    console.error('❌ Failed to send Telegram message:', error.message);
+  }
 };
