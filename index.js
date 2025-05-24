0e5c9187d4bfb9d46f96b266fe02227044796e12
nai golf <niy_golf@hotmail.com>
2025-05-24 21:55:19 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const WebSocket = require('ws');
+const { onPriceUpdate } = require('./tradeLogic');
+
+const symbol = process.env.SYMBOL.toLowerCase();
+const ws = new WebSocket(`wss://api.bitkub.com/websocket-api/market.ticker`);
+
+ws.on('open', () => {
+  console.log('[WS] Connected to Bitkub WebSocket');
+  ws.send(JSON.stringify({ event: 'subscribe', channel: `market.ticker.${symbol}` }));
+});
+
+ws.on('message', (data) => {
+  const res = JSON.parse(data);
+  if (res?.data?.last) {
+    const price = res.data.last;
+    onPriceUpdate(price);
+  }
+});
