45c817ea4a2d04207c7c53c03af4a94fc96d6d35
nai golf <niy_golf@hotmail.com>
2025-05-25 01:03:13 +0700

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,15 +1,10 @@
+// telegram.js
 const axios = require('axios');
 
-async function sendTelegram(botToken, chatId, message) {
-  try {
-    await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-      chat_id: chatId,
-      text: message,
-      parse_mode: 'Markdown',
-    });
-  } catch (e) {
-    console.error('Telegram send error:', e.message);
-  }
-}
-
-module.exports = sendTelegram;
+module.exports = async function sendTelegram(token, chatId, message) {
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+  await axios.post(url, {
+    chat_id: chatId,
+    text: message,
+  });
+};
